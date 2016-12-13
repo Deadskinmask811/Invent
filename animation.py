efcb0fae0088b1f4abbe7f835fdec8474ae10ae0
Coty Stover <cstover811@gmail.com>
2016-12-13 13:27:32 -0500

    adding some old files plus new spritesAndSounds project directory

--- animation.py
+++ animation.py
@@ -5,8 +5,8 @@ from pygame.locals import *
 pygame.init()
 
 # set up the window
-WINDOWWIDTH = 400
-WINDOWHEIGHT = 400
+WINDOWWIDTH = 900 
+WINDOWHEIGHT = 900 
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation')
 
@@ -16,19 +16,21 @@ DOWNRIGHT = 3
 UPLEFT = 7
 UPRIGHT = 9
 
-MOVESPEED = 4
+MOVESPEED = 8
 
 # set up the colors
 BLACK = (0,0,0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+ORANGE = (255, 144, 25)
 
 # set up teh block data structure
 b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
 b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
 b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
-blocks = [b1, b2, b3]
+b4 = {'rect':pygame.Rect(0, 0, 90, 10), 'color':ORANGE, 'dir':DOWNRIGHT}
+blocks = [b1, b2, b3, b4]
 
 # run the game loop
 while True:
@@ -87,4 +89,4 @@ while True:
 
     # draw the window on the screen
     pygame.display.update()
-    time.sleep(0.02)
+    time.sleep(0.00833)
